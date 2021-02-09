b1896136f8776af8c5e92e2c8ce759c4afcee8a3
Fernando Corrêa da Silva <fernando_silvasc@hotmail.com>
2021-02-09 10:55:08 -0800

    fixed toggleAvailable function

--- src/components/Food/index.tsx
+++ src/components/Food/index.tsx
@@ -29,11 +29,15 @@ const Food: React.FC<IProps> = ({
   const [isAvailable, setIsAvailable] = useState(food.available);
 
   async function toggleAvailable(): Promise<void> {
-    await api.put(`/foods/${food.id}`, {
-      available: !isAvailable,
-    });
+    try {
+      await api.put(`/foods/${food.id}`, {
+        available: !isAvailable,
+      });
 
-    setIsAvailable(!isAvailable);
+      setIsAvailable(!isAvailable);
+    } catch (err) {
+      console.log(err);
+    }
   }
 
   function setEditingFood(): void {
